c70d3054c30342108cc19885457fd4f7186cbffc
Max <agnusmaximus@berkeley.edu>
2016-06-18 16:03:43 -0700

    pin_to_core checks if _GNU_SOURCE is defined.

--- src/defines.h
+++ src/defines.h
@@ -60,10 +60,12 @@ public:
 };
 
 void pin_to_core(size_t core) {
-  //cpu_set_t cpuset;
-  //CPU_ZERO(&cpuset);
-  //CPU_SET(core, &cpuset);
-  //pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+#ifdef _GNU_SOURCE
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(core, &cpuset);
+    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+#endif
 }
 
 DEFINE_string(data_file, "blank", "Input data file.");
