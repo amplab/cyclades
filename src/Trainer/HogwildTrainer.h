0bb5dbea7d870f4c635dfda2c48d85d82a0d7861
Max <agnusmaximus@berkeley.edu>
2016-08-25 01:37:09 -0700

    Add random per batch datapoint processing for hogwild

--- src/Trainer/HogwildTrainer.h
+++ src/Trainer/HogwildTrainer.h
@@ -18,6 +18,19 @@ public:
 
 	model->SetUpWithPartitions(partitions);
 
+	// Default datapoint processing ordering.
+	// [thread][batch][index].
+	std::vector<std::vector<std::vector<int> > > per_batch_datapoint_order(FLAGS_n_threads);
+	for (int thread = 0; thread < FLAGS_n_threads; thread++) {
+	    per_batch_datapoint_order[thread].resize(partitions.NumBatches());
+	    for (int batch = 0; batch < partitions.NumBatches(); batch++) {
+		per_batch_datapoint_order[thread][batch].resize(partitions.NumDatapointsInBatch(thread, batch));
+		for (int index = 0; index < partitions.NumDatapointsInBatch(thread, batch); index++) {
+		    per_batch_datapoint_order[thread][batch][index] = index;
+		}
+	    }
+	}
+
 	// Train.
 	Timer gradient_timer;
 	for (int epoch = 0; epoch < FLAGS_n_epochs; epoch++) {
@@ -25,12 +38,23 @@ public:
 		this->PrintTimeLoss(gradient_timer, model, datapoints);
 	    }
 
+	    // Random per batch datapoint processing.
+	    if (FLAGS_random_per_batch_datapoint_processing) {
+		for (int thread = 0; thread < FLAGS_n_threads; thread++) {
+		    int batch = 0;
+		    for (int index = 0; index < partitions.NumDatapointsInBatch(thread, batch); index++) {
+			per_batch_datapoint_order[thread][batch][index] = rand() % partitions.NumDatapointsInBatch(thread, batch);
+		    }
+		}
+	    }
+
 	    updater->EpochBegin();
 
 #pragma omp parallel for schedule(static, 1)
 	    for (int thread = 0; thread < FLAGS_n_threads; thread++) {
 		int batch = 0; // Hogwild only has 1 batch.
-		for (int index = 0; index < partitions.NumDatapointsInBatch(thread, batch); index++) {
+		for (int index_count = 0; index_count < partitions.NumDatapointsInBatch(thread, batch); index_count++) {
+		    int index = per_batch_datapoint_order[thread][batch][index_count];
 		    updater->UpdateWrapper(model, partitions.GetDatapoint(thread, batch, index), thread);
 		}
 	    }
